53e1694384f7bd5a9d1b8aa61f6944e993bef822
Sar Champagne Bielert <sar@codepath.org>
2024-03-12 10:02:46 -0600

    Unit 1 should download files even if wireshark is installed

--- Scripts/unit1_lab.sh
+++ Scripts/unit1_lab.sh
@@ -6,31 +6,31 @@ scripts_repo="https://raw.githubusercontent.com/codepath/cyb102-vm-setup/main/Fi
 
 echo "[UNIT 1 LAB] Starting script..."
 
-# Check if the script needs to run
+# Check if the Wireshark needs to be installed
 if command -v wireshark >/dev/null 2>&1 ; then
     echo -e "${green}[UNIT 1 LAB]${none} Wireshark is already installed."
-    exit 0
-fi
-echo "[UNIT 1 LAB] Installing Wireshark..."
-
-# Pre-set the answer for the wireshark-common package
-echo "wireshark-common wireshark-common/install-setuid boolean true" | sudo debconf-set-selections
-
-# Add the Wireshark PPA
-sudo add-apt-repository -y ppa:wireshark-dev/stable
-
-# Update the package list
-sudo apt-get update
-
-# Install Wireshark in non-interactive mode
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y wireshark
-
-# Print output based on whether or not Wireshark is installed
-if command -v wireshark >/dev/null 2>&1 ; then
-    echo -e "${green}[UNIT 1 LAB]${none} Wireshark installed successfully."
 else
-    echo -e "${red}[UNIT 1 LAB]${none} ERROR: Wireshark was not installed correctly!"
-    exit 1
+    echo "[UNIT 1 LAB] Installing Wireshark..."
+
+    # Pre-set the answer for the wireshark-common package
+    echo "wireshark-common wireshark-common/install-setuid boolean true" | sudo debconf-set-selections
+
+    # Add the Wireshark PPA
+    sudo add-apt-repository -y ppa:wireshark-dev/stable
+
+    # Update the package list
+    sudo apt-get update
+
+    # Install Wireshark in non-interactive mode
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y wireshark
+
+    # Print output based on whether or not Wireshark is installed
+    if command -v wireshark >/dev/null 2>&1 ; then
+        echo -e "${green}[UNIT 1 LAB]${none} Wireshark installed successfully."
+    else
+        echo -e "${red}[UNIT 1 LAB]${none} ERROR: Wireshark was not installed correctly!"
+        exit 1
+    fi
 fi
 
 # Ensure the lab_1 directory exists
